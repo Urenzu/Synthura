6f4a044c10d3e62f04d7ca12c8782d41489a851c
Owen Richard Arnst <oarnst@ucsc.edu>
2024-04-17 09:44:17 -0700

    updated sidebar for environment management

--- synthura/src/components/EnvironmentSideBar.jsx
+++ synthura/src/components/EnvironmentSideBar.jsx
@@ -4,10 +4,8 @@ import EnvironmentButton from "./EnvironmentButton"
 const EnvironmentSideBar = () => {
   return (
     <section id="side-bar">
-      <button id="environment-dropdown-btn"><span>Environments</span> <svg id="environment-dropdown-icon" xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 
-        192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
-      </button>
+      <h2>Manage Environments</h2>
+      <EnvironmentButton />
     </section>
   )
 }
