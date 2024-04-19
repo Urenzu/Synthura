419f0184a95369e617deaf02c5217b9f3dcfbdf2
Owen Richard Arnst <oarnst@ucsc.edu>
2024-04-18 17:52:32 -0700

    Created a sidebar that slides in/out of the page when clicking an icon

--- synthura/src/components/EnvironmentSideBar.jsx
+++ synthura/src/components/EnvironmentSideBar.jsx
@@ -2,14 +2,16 @@ import "./tempStyles/EnvironmentSideBar.css"
 import EnvironmentButton from "./EnvironmentButton"
 import ClusterButton from "./ClusterButton"
 
-const EnvironmentSideBar = () => {
+const EnvironmentSideBar = ({showSideBar}) => {
+
   return (
-    <section id="side-bar">
+    <section id="side-bar" className={showSideBar ? "show-side-bar" : "hide-side-bar"} >
       <h2>Manage Environments</h2>
       <div className="environment">
         <EnvironmentButton />
         <ClusterButton />
       </div>
+      <button id="add-environment-btn">Add Environment</button>
     </section>
   )
 }
--- synthura/src/pages/EnvironmentsPage.jsx
+++ synthura/src/pages/EnvironmentsPage.jsx
@@ -1,10 +1,24 @@
 import CameraGrid from '../components/CameraGrid';
 import EnvironmentSideBar from '../components/EnvironmentSideBar';
+import { useState } from "react";
 
 const EnvironmentsPage = () => {
+
+  const [ showSideBar, setShowSideBar ] = useState(false);
+
+  // show sidebar when the hamburger image is clicked
+  const toggleSideBar = () => {
+    setShowSideBar(!showSideBar);
+  };
+
   return (
       <main>
-        <EnvironmentSideBar />
+        <svg id="side-bar-icon" onClick={toggleSideBar} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 
+          14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 
+          0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+        </svg>
+        <EnvironmentSideBar showSideBar={showSideBar} />
         <CameraGrid />
       </main>
   );
