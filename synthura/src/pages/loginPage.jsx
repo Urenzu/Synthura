e6df1569d4ce287124ff9f17c02cc3be9839136b
Githika <gannapur@ucsc.edu>
2024-04-19 09:52:00 -0700

    google auth half working

--- synthura/src/pages/loginPage.jsx
+++ synthura/src/pages/loginPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { GoogleLogin } from '@react-oauth/google'
 import '../App.css'
-GOOGLE_OAUTH_CLIENT_ID='581189616604-pr82ronvl9asp6pmsb780qn3dm69crrq.apps.googleusercontent.com'
-GOOGLE_OAUTH_CLIENT_SECRET='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'
+//GOOGLE_OAUTH_CLIENT_ID='581189616604-pr82ronvl9asp6pmsb780qn3dm69crrq.apps.googleusercontent.com'
+//GOOGLE_OAUTH_CLIENT_SECRET='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'
 
 
 export default function LoginPage() {
