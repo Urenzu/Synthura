a0cedde256cee3d3ed7233e0f14a4a8739ef6ece
Githika <gannapur@ucsc.edu>
2024-04-19 09:59:20 -0700

    took out API keys (so login wont work)

---
+++ synthura/src/pages/loginPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { GoogleLogin } from '@react-oauth/google'
+import '../App.css'
+
+export default function LoginPage() {
+    console.log("in login page")
+    return (
+      <>
+        <h2>Sign in to Synthura</h2>
+
+        <GoogleLogin className="google-auth"
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+      </>
+    )
+}
