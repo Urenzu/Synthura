fc6ec33c3df5bde85c5e8bbdb07e4bf326c430f0
Githika <gannapur@ucsc.edu>
2024-04-19 09:46:19 -0700

    google auth

--- synthura/src/main.jsx
+++ synthura/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="581189616604-pr82ronvl9asp6pmsb780qn3dm69crrq.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
