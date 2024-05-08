90b00bcbda3d7ecae17eb8b5d2505c3d4b7d86ac
Owen Richard Arnst <oarnst@ucsc.edu>
2024-05-08 12:50:05 -0700

    checkpoint

---
+++ synthura/src/components/CameraGrid/CameraGrid.test.js
@@ -0,0 +1,68 @@
+// ButtonComponent.test.js
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import CameraGrid from './CameraGrid';
+
+// Mock RTCPeerConnection
+class MockRTCPeerConnection {
+    constructor() {}
+    // Add any necessary methods or properties here
+}
+global.RTCPeerConnection = MockRTCPeerConnection;
+
+// Mock fetch() definition
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    status: 200,
+    ok: true,
+    json: () => Promise.resolve({}), // Mock JSON response
+  })
+);
+
+// Test 1 - (Add and remove one VideoFrame)
+test('VideoFrame renders upon button click', async () => {
+
+  // Arrange ----------------
+
+  const { getByTestId, queryByTestId } = render(<CameraGrid />);
+  expect(queryByTestId("1")).toBeFalsy();
+
+  // Act --------------------
+
+  const inputElement = getByTestId('cameraIP-test');
+  fireEvent.change(inputElement, { target: { value: 'mock-url' } });
+  const button = getByTestId('add-camera-btn-test');
+  fireEvent.click(button);
+
+  // Assert -----------------
+  
+  expect(queryByTestId("1")).toBeTruthy();
+
+});
+
+// Test 2 - (Remove one video frame)
+test('VideoFrame unrenders upon button click', async () => {
+
+  // Arrange ----------------
+
+  const { getByTestId, queryByTestId } = render(<CameraGrid />);
+  expect(queryByTestId("1")).toBeFalsy();
+
+  // Act --------------------
+
+  // Add a video frame
+  const inputElement = getByTestId('cameraIP-test');
+  fireEvent.change(inputElement, { target: { value: 'mock-url' } });
+  const add_btn = getByTestId('add-camera-btn-test');
+  fireEvent.click(add_btn);
+
+  // Remove the video frame
+  const remove_btn = getByTestId('close-camera-button-1');
+  fireEvent.click(remove_btn);
+
+  // Assert -----------------
+
+  expect(queryByTestId("1")).toBeFalsy();
+
+});
