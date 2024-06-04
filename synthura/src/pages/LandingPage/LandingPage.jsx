af980c68114e2f7a404a9a6e9f058007d619a6cf
Owen Richard Arnst <oarnst@ucsc.edu>
2024-06-03 23:59:25 -0700

    login page frontend

--- synthura/src/pages/LandingPage/LandingPage.jsx
+++ synthura/src/pages/LandingPage/LandingPage.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
-import { GoogleLogin } from '@react-oauth/google'
 import { Link } from 'react-router-dom';
 import './LandingPage.css'
 
 const LandingPage = () => {
   return (
     <main>
-      <nav>
-        <Link className="nav-link" to="/main">Main Page</Link>
-        <Link className="nav-link" to="/recordings">Recordings Page</Link>
-      </nav>
       <h1>Synthura</h1>
-      <p>Blurb about product. Blurb about product. Blurb about product. Blurb about product. Blurb about product. </p>
-      <GoogleLogin
-        onSuccess={credentialResponse => {
-          console.log(credentialResponse);
-        }}
-        onError={() => {
-          console.log('Login Failed');
-        }}
-      />
+      <p><strong>ENHANCE</strong> Your Security with <strong>CUTTING-EDGE AI</strong> Object Detection</p>
+      <p>Monitor Live Video from Everyday Devices to Advanced Cameras</p>
+      <form id="login-container">
+        <h2>Login To Get Started</h2>
+        <div id="user-information-fields">
+          <h3>Username</h3>
+          <input type="text" />
+          <h3>Password</h3>
+          <input type="password" />
+          <p>
+            New to Synthura? Sign up <Link id="register-link" to="/register">Here</Link>
+          </p>
+        </div>
+        <span></span>
+        <button id="login-button" type="submit">Login</button>
+      </form>
     </main>
   );
 }
